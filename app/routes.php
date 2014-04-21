d3c71408a48def00a8b6331ceac2c6b65e60a22d
cmcclees <cmcclees@Cameron-McCleess-MacBook.local>
2014-04-21 13:57:26 -0700

    changing routes
    making group that need authentication in routes file

--- app/routes.php
+++ app/routes.php
@@ -1,56 +1,45 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
-
-/*Route::get('/', function()
-{
-	return View::make('hello');
-});*/
-
-
-
-
 Route::get('login', array('as'=>'login','uses'=> 'LoginController@showLogin'));
 Route::post('login', array('as'=>'login','uses'=> 'LoginController@processLogin'));
 
 Route::get('register', array('as' => 'register', 'uses' => 'LoginController@register'));
 Route::post('register', array('as'=>'register', 'uses' => 'LoginController@processRegister'));
 
-Route::get('dashboard', array('as'=>'dashboard','uses'=>'MovieController@dashboard'))->before('auth');
-
-
-Route::resource('users', 'UserController');
-Route::resource('movies', 'MovieController');
+Route::get('logout', array('as'=>'logout','uses'=> 'LoginController@logout'));
 
 
 
-Route::get('logout', array('as'=>'logout','uses'=> 'LoginController@logout'))->before('auth');
+Route::group(array('before' => 'auth'), function() {
+    Route::resource('users', 'UserController');
+    Route::resource('movies', 'MovieController');
+
+    Route::get('dashboard', array('as'=>'dashboard','uses'=>'MovieController@dashboard'));
+
+    Route::get('movieSearch', array('as'=>'movieSearch','uses'=> 'MovieController@search'));
+    Route::get('watchedMovieList', array('as'=>'watchedMovieList','uses'=>'MovieController@listWatchedMovies'));
+
+    Route::post('searchedMovieList', array('as'=>'searchedMovieList','uses'=>'MovieController@listSearchedMovies'));
+
+    Route::post('createMovie', array('as'=>'createMovie', 'uses'=>'MovieController@createMovie'));
+    Route::get('createMovieManually', array('as'=>'createMovieManually', 'uses'=>'MovieController@createMovieManually'));
+
+
+    Route::post('insertMovie', array('as'=>'insertMovie', 'uses'=>'MovieController@insertMovie'));
+
+
+    Route::get('addMovie', array('as'=>'addMovie', 'uses'=>'ItunesController@movieSearch'));
+    Route::post('addMovie', array('as'=>'addMovie', 'uses'=>'ItunesController@movieProcess'));
+
+});
 
 
 
-Route::get('movieSearch', array('as'=>'movieSearch','uses'=> 'MovieController@search'))->before('auth');
-Route::get('watchedMovieList', array('as'=>'watchedMovieList','uses'=>'MovieController@listWatchedMovies'))->before('auth');
-
-Route::post('searchedMovieList', array('as'=>'searchedMovieList','uses'=>'MovieController@listSearchedMovies'))->before('auth');
-
-Route::post('createMovie', array('as'=>'createMovie', 'uses'=>'MovieController@createMovie'))->before('auth');
-Route::get('createMovieManually', array('as'=>'createMovieManually', 'uses'=>'MovieController@createMovieManually'))->before('auth');
 
 
-Route::post('insertMovie', array('as'=>'insertMovie', 'uses'=>'MovieController@insertMovie'))->before('auth');
 
 
-Route::get('addMovie', array('as'=>'addMovie', 'uses'=>'ItunesController@movieSearch'))->before('auth');
-Route::post('addMovie', array('as'=>'addMovie', 'uses'=>'ItunesController@movieProcess'))->before('auth');
+
 
 
 
